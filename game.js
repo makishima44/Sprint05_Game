961667eab4d25a081db31fa326f4318ffe344032
Artur <metallist15431543@gmail.com>
2024-07-24 23:40:05 +0300

    lesson_13_finished

--- game.js
+++ game.js
@@ -1,5 +1,52 @@
 export class Game {
-  #settings;
+  #settings = {
+    gridSize: {
+      columns: 3,
+      rows: 5,
+    },
+  };
+  #status = "pending";
+  #player1;
+  #player2;
+  #google;
+
+  #getRandomPosition(takenPosition = []) {
+    let newX;
+    let newY;
+
+    do {
+      newX = NumberUtil.getRandomNumber(this.#settings.gridSize.columns);
+      newY = NumberUtil.getRandomNumber(this.#settings.gridSize.rows);
+    } while (
+      takenPosition.some(
+        (position) => position.x === newX && position.y === newY
+      )
+    );
+
+    return new Position(newX, newY);
+  }
+
+  #createUnits() {
+    const player1Position = this.#getRandomPosition();
+    this.#player1 = new Player(1, player1Position);
+
+    const player2Position = this.#getRandomPosition([player1Position]);
+    this.#player2 = new Player(2, player2Position);
+
+    const googlePosition = this.#getRandomPosition([
+      player1Position,
+      player2Position,
+    ]);
+    this.#google = new Google(googlePosition);
+  }
+
+  start() {
+    if (this.#status === "pending") {
+      this.#status = "in-process";
+    }
+
+    this.#createUnits();
+  }
 
   set settings(settings) {
     this.#settings = settings;
@@ -7,5 +54,53 @@ export class Game {
 
   get settings() {
     return this.#settings;
+  }
+
+  get status() {
+    return this.#status;
+  }
+
+  get player1() {
+    return this.#player1;
+  }
+
+  get player2() {
+    return this.#player2;
+  }
+
+  get google() {
+    return this.#google;
+  }
+}
+
+class Units {
+  constructor(position) {
+    this.position = position;
+  }
+}
+
+export class Player extends Units {
+  constructor(id, position) {
+    super(position);
+    this.id = id;
+  }
+}
+
+export class Google extends Units {
+  constructor(position) {
+    super(position);
+  }
+}
+
+export class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class NumberUtil {
+  static getRandomNumber(max) {
+    return Math.floor(Math.random() * max + 1);
   }
 }
--- game.test.js
+++ game.test.js
@@ -7,13 +7,59 @@ describe("Game Tests", () => {
     game.settings = {
       gridSize: {
         columns: 10,
-        row: 10,
+        rows: 10,
       },
     };
 
     const settings = game.settings;
 
-    expect(settings.gridSize.row).toBe(10);
+    expect(settings.gridSize.rows).toBe(10);
     expect(settings.gridSize.columns).toBe(10);
+  });
+
+  it("should change status", () => {
+    const game = new Game();
+
+    game.settings = {
+      gridSize: {
+        columns: 10,
+        rows: 10,
+      },
+    };
+
+    expect(game.status).toBe("pending");
+    game.start();
+    expect(game.status).toBe("in-process");
+  });
+
+  //-------------------------------------------------------------//
+
+  it("should units have unique position", () => {
+    for (let i = 0; i < 10; i++) {
+      const game = new Game();
+
+      game.settings = {
+        gridSize: {
+          columns: 4,
+          rows: 3,
+        },
+      };
+
+      game.start();
+      expect([1, 2, 3, 4]).toContain(game.player1.position.x);
+      expect([1, 2, 3]).toContain(game.player1.position.y);
+
+      expect([1, 2, 3, 4]).toContain(game.player2.position.x);
+      expect([1, 2, 3]).toContain(game.player2.position.y);
+
+      expect(
+        (game.player1.position.x !== game.player2.position.x ||
+          game.player1.position.y !== game.player2.position.y) &&
+          (game.player1.position.x !== game.google.position.x ||
+            game.player1.position.y !== game.google.position.y) &&
+          (game.player2.position.x !== game.google.position.x ||
+            game.player2.position.y !== game.google.position.y)
+      ).toBe(true);
+    }
   });
 });
